52a898c34ae85034fb6ffb6a88ea78ee644358ec
Pedro Borges de Jesus <pedro.borges.jesus@gmail.com>
2024-03-04 23:35:56 -0300

    Mudança em toda tabela, adição de classe e métodos e opção de deletar incluída

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 class Produto{
 
-    constructor(){}
+    constructor(){
+        this.id = 1;
+        this.arrayProdutos = [];
+    }
 
-    VerificaCampo(nome){
-        if(nome == ''){
+    SalvarDados(){
+        let produto = this.LerDados();
+        if(this.VerificaCampo(produto)){
+            this.AdicionarNaArray(produto);
+            this.AdicionarNaTabela();
+        }
+        document.querySelector("#nomeProduto").value = "";
+    }
+
+    LerDados(){
+        let produto = {};
+        produto.id = this.id;
+        produto.nome = document.querySelector("#nomeProduto").value;
+        produto.tipo = this.RetornaTipo();
+
+        return produto;
+    }
+
+    VerificaCampo(produto){
+        if(produto.nome == ''){
             document.querySelector("small").innerHTML = "Preencha o campo!";
             return false;
         }
@@ -14,29 +35,82 @@ class Produto{
     }
 
     RetornaTipo(){
-        if(document.querySelector("#graos").checked == true){ return "grao"; }
-        else if(document.querySelector("#massas").checked == true) {return "massa"; }
-        else if(document.querySelector("#verduras").checked == true){ return "verdura"; }
-        else if(document.querySelector("#frutas").checked == true){ return "fruta"; }
-        else if(document.querySelector("#laticinios").checked == true){ return "laticinio"; }
-        else if(document.querySelector("#congelados").checked == true){ return "congelado"; }
-        else if(document.querySelector("#doces").checked == true){ return "doce"; }
-        else if(document.querySelector("#limpezas").checked == true){ return "limpeza"; }
-        else{ return "naoEspecificado" }
+        if(document.querySelector("#graos").checked == true){ 
+            document.querySelector("#graos").checked = false;
+            return "Grão"; 
+        }
+        else if(document.querySelector("#massas").checked == true) {
+            document.querySelector("#massas").checked = false;
+            return "Massa"; 
+        }
+        else if(document.querySelector("#verduras").checked == true){
+            document.querySelector("#verduras").checked = false;
+            return "Verdura"; 
+        }
+        else if(document.querySelector("#frutas").checked == true){
+            document.querySelector("#frutas").checked = false;
+            return "Fruta";
+        }
+        else if(document.querySelector("#laticinios").checked == true){
+            document.querySelector("#laticinios").checked = false;
+            return "Laticínio";
+        }
+        else if(document.querySelector("#congelados").checked == true){
+            document.querySelector("#congelados").checked = false;
+            return "Congelado";
+        }
+        else if(document.querySelector("#doces").checked == true){
+            document.querySelector("#doces").checked = false;
+            return "Doce";
+        }
+        else if(document.querySelector("#limpezas").checked == true){
+            document.querySelector("#limpezas").checked = false;
+            return "Limpeza";
+        }
+        else{ 
+            return "Não Especificado" 
+        }
     }
 
-    Adicionar(){
-        let produto = {};
-        produto.nome = document.querySelector("#nome_produto").value;
-        produto.tipo = this.RetornaTipo();
+    AdicionarNaArray(produto){
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
 
-        let campoPrenchido = this.VerificaCampo(produto.nome);
-        if(campoPrenchido == true){
-            let p = document.createElement("p");
-            p.innerHTML = produto.nome;
-            document.querySelector(`#coluna_${produto.tipo}`).appendChild(p);
-            document.querySelector("#nome_produto").value = "";
-            document.querySelector(`#${produto.tipo}s`).checked = false;
+    AdicionarNaTabela(){
+        let tbody = document.querySelector("tbody");
+        tbody.innerText = "";
+
+        for(let i=0; i<this.arrayProdutos.length; i++){
+            let linha = tbody.insertRow();
+            let colunaId = linha.insertCell();
+            let colunaNome = linha.insertCell();
+            let colunaTipo = linha.insertCell();
+            let colunaAcao = linha.insertCell();
+
+            colunaId.innerHTML = this.arrayProdutos[i].id;
+            colunaNome.innerHTML = this.arrayProdutos[i].nome;
+            colunaTipo.innerHTML = this.arrayProdutos[i].tipo;
+            
+            let imgDelete = document.createElement("img");
+            imgDelete.src = "img/delete.svg";
+            imgDelete.style.width = "20px";
+            imgDelete.style.cursor = "pointer";
+
+            imgDelete.setAttribute("onclick", `produto.Deletar(${this.arrayProdutos[i].id})`);
+
+            colunaAcao.appendChild(imgDelete);
+        }
+        console.log(this.arrayProdutos);
+    }
+
+    Deletar(id){
+        let tbody = document.querySelector("tbody");
+        for(let i=0; i<this.arrayProdutos.length; i++){
+            if(id==this.arrayProdutos[i].id){
+                this.arrayProdutos.splice(i, 1);
+                tbody.deleteRow(i);
+            }
         }
     }
 }
